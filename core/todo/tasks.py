d5b2c6b9c39204171cba2d181058cfdbfc4330c3
AliBahrami <alifbahrami13766@gmail.com>
2026-02-07 14:55:29 +0330

    change automate in beat

--- core/todo/tasks.py
+++ core/todo/tasks.py
@@ -2,6 +2,6 @@ from celery import shared_task
 from .models import Task
 
 
-@shared_task
+@shared_task(name="todo.tasks.clear_done_tasks")
 def clear_done_tasks():
     Task.objects.filter(complete=True).delete()
